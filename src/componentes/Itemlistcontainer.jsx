ab5743a5b8acf16350aeadeb62b73401c16d8fd3
Ramiro <ramifazio@gmail.com>
2022-11-01 11:58:00 -0300

    Sugerencias!

--- src/componentes/Itemlistcontainer.jsx
+++ src/componentes/Itemlistcontainer.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import '../estilos/EstilosTitulos.css';
-export default function Itemlistcontainer(greeting) {
+export default function Itemlistcontainer({ greeting }) {
   return (
     <div>
-      <h1 className="Titulos">Bienvenidos a Cybertron</h1>
+      <h1 className="Titulos">{greeting}</h1> //* Aca podemos usar las props que nos llegan de esta manera!
     </div>
   );
 }
